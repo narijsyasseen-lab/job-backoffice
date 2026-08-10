3ffc990851334925fd2b4989365e55e9e822e11c
Narjis Yassin Mustafa <narijsyasseen@gmail.com>
2026-08-10 13:49:53 +0300

    project part2/4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,22 +15,25 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="flex">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="flex-1 min-h-screen bg-gray-100">            
+              <!-- Page Heading -->
+               @isset($header)
+                   <header class="bg-white shadow">
+                      <div class="py-4 px-4 w-full">
+                           {{ $header }}
+                      </div>
+                   </header>
+               @endisset
+
+              <!-- Page Content -->
+                <main>
+                   {{ $slot }}
+                </main>
+            </div>
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,24 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobVacancyController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanyController;
 
 
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [ DashboardController::class,'index'])->name('dashboard');
+    Route::get('/company', [ CompanyController::class,'index'])->name('company.index');
+    Route::get('/job-application', [ JobApplicationController::class,'index'])->name('application.index');
+    Route::get('/job-category', [ JobCategoryController::class,'index'])->name('job-category.index');
+    Route::get('/job-vacancy', [ JobVacancyController::class,'index'])->name('job-vacancy.index');
+    Route::get('/user', [ UserController::class,'index'])->name('user.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
